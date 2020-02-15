cf6d150bde1294a0e8e1d85d20e6d3368ecffc90
thenatural86 <philip.smalls86@gmail.com>
2020-02-15 07:39:50 -0500

    state added

--- src/index.js
+++ src/index.js
@@ -2,12 +2,21 @@ import React from "react"
 import ReactDOM from "react-dom"
 
 class App extends React.Component {
-  render() {
+  constructor(props) {
+    super(props)
+
+    this.state = { lat: null }
+
     window.navigator.geolocation.getCurrentPosition(
-      position => console.log(position),
+      position => {
+        this.setState({ lat: position.coords.latitude })
+      },
       err => console.log(err)
     )
-    return <div>Latitude: </div>
+  }
+
+  render() {
+    return <div>Latitude: {this.state.lat}</div>
   }
 }
 
