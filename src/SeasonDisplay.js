92673c73911ca8c9a3784bb8baec84c20306f29c
thenatural86 <philip.smalls86@gmail.com>
2020-02-17 15:24:55 -0500

    season logic

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,17 @@
 import React from "react"
 
-const SeasonDisplay = () => {
-  return <div>SeasonDisplay</div>
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "summer" : "winter"
+  } else {
+    return lat > 0 ? "winter" : "summer"
+  }
+}
+
+const SeasonDisplay = ({ lat }) => {
+  const season = getSeason(lat, new Date().getMonth())
+  console.log(season)
+  return <div>SeasonDisplay: {lat}</div>
 }
 
 export default SeasonDisplay
